c65a200647866b81794b5b23d4916d131b73ceaa
Huaiyuan Jing <ls.hyjing@gmail.com>
2024-12-25 10:45:22 -0600

    * fix(p3218 I.rs): fix type conversion issues and add explicit type annotations * feat(p3219 II.rs): add new file with the same logic as p3218 I.rs but with i64 type for ans variable * refactor(main.rs): refactor main function to calculate binomial coefficient using closures

--- src/bin/p3219切蛋糕的最小总开销II.rs
+++ src/bin/p3219切蛋糕的最小总开销II.rs
@@ -1,36 +1,36 @@
 struct Solution;
 impl Solution {
-    pub fn minimum_cost(m: i32, n: i32, horizontal_cut: Vec<i32>, vertical_cut: Vec<i32>) -> i32 {
+    pub fn minimum_cost(m: i32, n: i32, horizontal_cut: Vec<i32>, vertical_cut: Vec<i32>) -> i64 {
         let m = m as usize;
         let n = n as usize;
         let mut horizontal_cut = horizontal_cut;
         let mut vertical_cut = vertical_cut;
         horizontal_cut.sort_unstable_by(|a, b| b.cmp(a));
         vertical_cut.sort_unstable_by(|a, b| b.cmp(a));
-        let mut ans = 0;
+        let mut ans = 0 as i64;
         let mut horizontal_coefficient = 1;
         let mut vertical_coefficient = 1;
         let mut p_horizontal = 0;
         let mut p_vertical = 0;
-        for i in 0..(n - 1 + m - 1) {
+        for _ in 0..(n - 1 + m - 1) {
             if p_horizontal >= m - 1 {
-                ans += vertical_coefficient * vertical_cut[p_vertical];
+                ans += vertical_coefficient as i64 * vertical_cut[p_vertical] as i64;
                 p_vertical += 1;
                 horizontal_coefficient += 1;
                 continue;
             }
             if p_vertical >= n - 1 {
-                ans += horizontal_coefficient * horizontal_cut[p_horizontal];
+                ans += horizontal_coefficient as i64 * horizontal_cut[p_horizontal] as i64;
                 p_horizontal += 1;
                 vertical_coefficient += 1;
                 continue;
             }
             if horizontal_cut[p_horizontal] > vertical_cut[p_vertical] {
-                ans += horizontal_coefficient * horizontal_cut[p_horizontal];
+                ans += horizontal_coefficient as i64 * horizontal_cut[p_horizontal] as i64;
                 p_horizontal += 1;
                 vertical_coefficient += 1;
             } else {
-                ans += vertical_coefficient * vertical_cut[p_vertical];
+                ans += vertical_coefficient as i64 * vertical_cut[p_vertical] as i64;
                 p_vertical += 1;
                 horizontal_coefficient += 1;
             }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let factorial = |n: i32| {
+        let mut res = 1;
+        for i in 1..=n {
+            res *= i;
+        }
+        res
+    };
+    let bion = |n: i32, p: f32, k: i32| {
+        let res = factorial(n) / (factorial(k) * factorial(n - k));
+        res as f32 * p.powf(k as f32) * (1.0 - p).powf((n - k) as f32)
+    };
+    println!("{}", bion(5, 0.01, 3) / (bion(5, 0.01, 3) + bion(5, 0.01, 4) + bion(5, 0.01, 5)));
 }
