8407cf9ec4d6c6605a31d2330e0362f80e3232bf
Huaiyuan Jing <ls.hyjing@gmail.com>
2024-12-27 10:30:16 -0600

    * fix(p3159.rs): fix occurrences_of_element function to correctly handle queries that exceed record length * refactor(p3159.rs): optimize occurrences_of_element function for better performance

--- src/bin/p3159查询数组中元素的出现位置.rs
+++ src/bin/p3159查询数组中元素的出现位置.rs
@@ -1,8 +1,20 @@
 struct Solution;
 impl Solution {
     pub fn occurrences_of_element(nums: Vec<i32>, queries: Vec<i32>, x: i32) -> Vec<i32> {
-        
-        vec![]
+        let mut record = vec![];
+        for i in 0..nums.len() {
+            if nums[i] != x { continue; }
+            record.push(i as i32);
+        }
+        let mut res = vec![];
+        for i in 0..queries.len() {
+            if queries[i] as usize > record.len() {
+                res.push(-1);
+                continue;
+            }
+            res.push(record[queries[i] as usize - 1]);
+        }
+        res
     }
 }
 fn main() {}
