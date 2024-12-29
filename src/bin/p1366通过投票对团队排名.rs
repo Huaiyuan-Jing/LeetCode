c8106cae91fd40497a19f4c894fdc387c4d445be
Huaiyuan Jing <ls.hyjing@gmail.com>
2024-12-29 12:19:39 -0600

    * refactor(p1366.rs): Remove unnecessary struct declaration in Solution * fix(p1366.rs): Fix formatting and indentation in Solution implementation * refactor(p1366.rs): Remove unnecessary main function

--- src/bin/p1366通过投票对团队排名.rs
+++ src/bin/p1366通过投票对团队排名.rs
@@ -1,6 +1,5 @@
 use std::collections::hash_set::HashSet;
 
-struct Solution;
 impl Solution {
     pub fn rank_teams(mut votes: Vec<String>) -> String {
         let mut tickets = vec![vec![0; 26]; 26];
@@ -9,9 +8,10 @@ impl Solution {
                 tickets[(c as u8 - b'A' as u8) as usize][i] -= 1;
             }
         }
-        let ans = unsafe {votes[0].as_bytes_mut()};
+        let ans = unsafe { votes[0].as_bytes_mut() };
         ans.sort_unstable_by_key(|&c| (&tickets[(c - b'A') as usize], c));
         votes[0].clone()
     }
 }
+struct Solution;
 fn main() {}
